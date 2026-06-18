fde7623f6526ef1425ae882d08bd9a12f52c7f1b
Miro Kropacek <miro.kropacek@gmail.com>
2026-06-18 14:45:59 +1000

    Download cross libraries over HTTP instead of SSH

--- .scripts/install-libs.sh
+++ .scripts/install-libs.sh
@@ -3,17 +3,12 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
-DOWNLOAD_DIR=web196@server43.webgo24.de:/home/www/download/mint
+DOWNLOAD_DIR=http://tho-otto.de/download/mint
 SYSROOT_DIR=${SYSROOT_DIR:-"/"}
 
 sudo mkdir -p "${SYSROOT_DIR}"
 
-for package in $*
+for filename in $*
 do
-	scp -o "StrictHostKeyChecking no" "$DOWNLOAD_DIR/${package}-*mint-*dev*" .
-	f=`ls ${package}-*`
-	if test -f "$f"; then
-		sudo tar -C "${SYSROOT_DIR}" -xf "$f"
-		rm -f $f
-	fi
+	wget -q -O - "$DOWNLOAD_DIR/${filename}" | sudo tar -C "${SYSROOT_DIR}" -xJf -
 done
